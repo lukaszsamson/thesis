1db84d794aac332afbd36c9b3d40ed6f43d2110e
Lukasz Samson <lukaszsamson@gmail.com>
2012-03-09 06:12:23 +0100

    save or update

--- app/controllers/pages_controller.js
+++ app/controllers/pages_controller.js
@@ -1,41 +1,42 @@
-var locomotive = require('locomotive')
-  , Controller = locomotive.Controller
-  , Graph = require('../utils/graph-client.js').Graph
-  , Q = require('q')
-  , Person = require('../models/person.js')
-  , Link = require('../models/link.js')
-  , util = require('util');
+var locomotive = require('locomotive'),
+  Controller = locomotive.Controller,
+  Graph = require('../utils/graph-client.js').Graph,
+  Q = require('q'),
+  Person = require('../models/person.js'),
+  Link = require('../models/link.js'),
+  util = require('util');
 
 
 var kue = require('kue');
 
 // create our job queue
-
-var jobs = kue.createQueue()
-  , Job = kue.Job;
+var jobs = kue.createQueue(),
+  Job = kue.Job;
 
 var PagesController = new Controller();
 
-PagesController.test = function() {
+PagesController.test = function () {
   var self = this;
   self.title = 'thest';
-  getFriends(self.getToken());
-
-  self.render({
-    token: self.getToken()
-  });
+  getFriends(function(e) {
+    if (e)
+      return self.error(e);
+    self.render({
+      token: self.getToken()
+    });
+  }, self.getToken());
 }
 
 function NotFound(msg) {
-    Error.call(this);
-    Error.captureStackTrace(this, arguments.callee);
-    this.name = 'NotFound';
-    this.message = msg;
+  Error.call(this);
+  Error.captureStackTrace(this, arguments.callee);
+  this.name = 'NotFound';
+  this.message = msg;
 }
 
 NotFound.prototype.__proto__ = Error.prototype;
 
-PagesController.main = function() {
+PagesController.main = function () {
   var self = this;
 
 
@@ -44,43 +45,37 @@ PagesController.main = function() {
 
 
   if (!self.req.session.facebookToken && !code) {
-      this.redirect((new GraphClient).getDialogUrl());
-    } else {
-      var access_token = 
-      Q.when((function() {
+    this.redirect((new GraphClient).getDialogUrl());
+  } else {
+    var access_token = Q.when((function () {
 
-      })())
-      .then(function(access_token) {
-        return promissedRequest(getFriendsUrl(access_token));
-      })
-      .then(function(body) {
-        self.title = 'Locomotive';
-        var friends = JSON.parse(body).data;
+    })()).then(function (access_token) {
+      return promissedRequest(getFriendsUrl(access_token));
+    }).then(function (body) {
+      self.title = 'Locomotive';
+      var friends = JSON.parse(body).data;
 
-        friends.forEach(function(friend) {
-          create(friend, self.getToken());
-        });
-
-        self.render({
-          friends: friends
-        });
-      })
-      .fail(function(error) {
-        self.error(error);
+      friends.forEach(function (friend) {
+        create(friend, self.getToken());
       });
+
+      self.render({
+        friends: friends
+      });
+    }).fail(function (error) {
+      self.error(error);
+    });
   }
 }
 
-PagesController.friend = function() {
+PagesController.friend = function () {
   var self = this;
   var facebookId = self.req.params.id;
   Person.findOne({
     facebookId: facebookId
-  }, function(error, result) {
-    if (error)
-      return self.error(error);
-    if (!result)
-      return self.error(new NotFound(util.format('Friend with id %s does not exist', facebookId)));
+  }, function (error, result) {
+    if (error) return self.error(error);
+    if (!result) return self.error(new NotFound(util.format('Friend with id %s does not exist', facebookId)));
     self.render({
       facebookId: facebookId,
       links: result.links
@@ -88,21 +83,19 @@ PagesController.friend = function() {
   });
 }
 
-PagesController.friends = function() {
+PagesController.friends = function () {
   var self = this;
-  Person.find(function(error, result) {
-    if (error)
-      return self.error(error);
+  Person.find(function (error, result) {
+    if (error) return self.error(error);
     self.render({
       friends: result
     });
   });
 }
 
-PagesController.getToken = function() {
+PagesController.getToken = function () {
   var self = this;
-  if (self.req.session.facebookToken)
-    return self.req.session.facebookToken.access_token;
+  if (self.req.session.facebookToken) return self.req.session.facebookToken.access_token;
 }
 /*
 PagesController.getFriends = function() {
@@ -117,70 +110,88 @@ PagesController.getFriends = function() {
 }
 */
 
-function getFriends(access_token) {
+function getFriends(done, access_token) {
   console.log('Creating getFriends job');
   jobs.create('get friends', {
-      title: 'Getting friends'
-    , access_token: access_token
+    title: 'Getting friends',
+    access_token: access_token
     //, facebookId: facebookId
-  }).save();
+  }).attempts(3).save(done);
 }
 
-function getMutualFriends(friend, access_token) {
+function getMutualFriends(done, friend, access_token) {
   console.log('Creating getMutualFriends job for %s', friend.name);
   jobs.create('get mutual friends', {
-      title: 'Getting mutual friends of ' + friend.name
-    , friendFacebookId: friend.id
-    , friendName: friend.name
-    , access_token: access_token
+    title: 'Getting mutual friends of ' + friend.name,
+    friendFacebookId: friend.id,
+    friendName: friend.name,
+    access_token: access_token
     //, facebookId: facebookId
-  }).save();
+  }).attempts(3).save(done);
 }
 
-function getLinks(friend, access_token) {
+function getLinks(done, friend, access_token) {
   console.log('Creating getLinks job for %s', friend.name);
   jobs.create('get links', {
-      title: 'Getting links submitted by ' + friend.name
-    , friendFacebookId: friend.id
-    , friendName: friend.name
-    , access_token: access_token
+    title: 'Getting links submitted by ' + friend.name,
+    friendFacebookId: friend.id,
+    friendName: friend.name,
+    access_token: access_token
     //, facebookId: facebookId
-  }).save();
+  }).attempts(3).save(done);
 }
 
-jobs.process('get friends', 3, function(job, done) {
-  (new Graph(job.data.access_token))
-  .getFriends()
-  .then(function(friends) {
-    friends.forEach(function(friend) {
+function saveOrUpdateFriend(done, friend, access_token) {
+  jobs.create('save or update friend', {
+    title: 'Saving or updating friend ' + friend.name,
+    friend: friend,
+    access_token: access_token
+  }).priority('high').attempts(3).save(done);
+}
+
+jobs.process('save or update friend', 3, function (job, done) {
+  var friend = job.data.friend;
+  Person.findOne({
+    facebookId: friend.id
+  }, function (error, result) {
+    if (error) return done(error);
+    if (!result) {
       var model = new Person({
         name: friend.name,
         facebookId: friend.id,
         queriedDate: new Date(),
         links: []
-      }).save(function(error) {
-        if (error) {
-          console.log("Save to db failed");
-          return;
+      }).save(function (error1) {
+        if (error1) {
+          return done(error1);
+        } else {
+          getLinks(function(e) {}, friend, job.data.access_token);
+          getMutualFriends(function(e) {}, friend, job.data.access_token);
+          done();
         }
-        getLinks(friend, job.data.access_token);
-        getMutualFriends(friend, job.data.access_token);
       });
-    });
-    done();
-  })
-  .fail(function(error) {
-    console.log('Error while getting friends');
-    console.log(error);
-    done(error);
-  })
+    } else {
+      getLinks(friend, job.data.access_token);
+      getMutualFriends(friend, job.data.access_token);
+      done();
+    }
+  });
 });
 
-jobs.process('get mutual friends', 3, function(job, done) {
-  (new Graph(job.data.access_token))
-  .getMutualFriends(job.data.friendFacebookId)
-  .then(function(friends) {
-    friends.forEach(function(friend) {
+jobs.process('get friends', 3, function (job, done) {
+  (new Graph(job.data.access_token)).getFriends().then(function (friends) {
+    friends.forEach(function (friend) {
+      saveOrUpdateFriend(friend, job.data.access_token)
+    });
+    done();
+  }).fail(function (error) {
+    done(error);
+  });
+});
+
+jobs.process('get mutual friends', 3, function (job, done) {
+  (new Graph(job.data.access_token)).getMutualFriends(job.data.friendFacebookId).then(function (friends) {
+    friends.forEach(function (friend) {
       /*var model = new Person({
         name: friend.name,
         facebookId: friend.id,
@@ -196,23 +207,19 @@ jobs.process('get mutual friends', 3, function(job, done) {
       });*/
     });
     done();
-  })
-  .fail(function(error) {
-    console.log('Error while getting friends');
-    console.log(error);
+  }).fail(function (error) {
     done(error);
   })
 });
 
-jobs.process('get links', 3, function(job, done) {
-  (new Graph(job.data.access_token)).getLinks(job.data.friendFacebookId)
-  .then(function(links) {
+jobs.process('get links', 3, function (job, done) {
+  (new Graph(job.data.access_token)).getLinks(job.data.friendFacebookId).then(function (links) {
     console.log('got %d links by %s', links.length, job.data.friendName);
     Person.update({
       facebookId: job.data.friendFacebookId
     }, {
       $pushAll: {
-        links: links.map(function(link) {
+        links: links.map(function (link) {
           return new Link({
             url: link.link,
             facebookId: link.id
@@ -221,28 +228,22 @@ jobs.process('get links', 3, function(job, done) {
       }
     }, {
       multi: false
-    }, function(error, numAffected) {
-      console.log('in callback');
-      console.log(error);
-      console.log(numAffected);
+    }, function (error, numAffected) {
       done(error);
     });
-  })
-  .fail(function(error) {
-    console.log('Error while getting links by %s', job.data.friendName);
-    console.log(error);
+  }).fail(function (error) {
     done(error);
   });
 });
 
-jobs.on('job complete', function(id){
-  Job.get(id, function(err, job){
+jobs.on('job complete', function (id) {
+  Job.get(id, function (err, job) {
     if (err) {
       console.log('Error while getting job #%d', job.id);
       console.log(err);
       return;
     }
-    job.remove(function(err){
+    job.remove(function (err) {
       if (err) {
         console.log('Error while getting job #%d', job.id);
         console.log(err);
@@ -300,8 +301,6 @@ jobs.on('job complete', function(id){
 // }
 // };
 
-
-
 /*    var path = 'https://graph.facebook.com/oauth/access_token?';
     var queryParams = [
       'client_id=' + APP_ID,
@@ -342,5 +341,4 @@ jobs.on('job complete', function(id){
 }
 */
 //}
-
 module.exports = PagesController;
